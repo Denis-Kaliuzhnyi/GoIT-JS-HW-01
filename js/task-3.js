d92fcbc9bb52eddb4f22ece166bacd0c4da84685
Denis Kaliuzhnyi <deniskl4200@gmail.com>
2024-03-08 21:16:17 +0200

    end HW-1

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,15 @@
-let getElementWidth = function (content, padding, border) {
-    content = Number.parseFloat;
-    let padding = Number.parseFloat;
-    let border = Number.parseFloat;
-    
-    return content + padding +border;
+
+function getElementWidth (content, padding, border) {
+    content = parseFloat(content);
+    padding = parseFloat(padding);
+    border = parseFloat(border);
+    let getElementWidth = content + (padding * 2) + (border * 2);
+    return getElementWidth;
 }
+
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 
-
-
-
-/*
-console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+
 console.log(getElementWidth("200px", "0px", "0px")); // 200
-*/
 
